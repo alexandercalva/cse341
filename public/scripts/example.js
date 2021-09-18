059053a50a519b43079d257e6a2fad637ba2541b
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2021-09-17 20:56:19 -0500

    Project-week1

---
+++ public/scripts/example.js
@@ -0,0 +1,6 @@
+/****************************************************
+ * You can use client side scripts by placing them in
+ * the public/scripts folder. They can then be linked
+ * to your EJS files and used there.
+ ****************************************************/
+console.log("nODE");
